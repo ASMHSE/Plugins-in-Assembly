83b2c31e76479a06d8430a139e17bcdae20cfed0
ASMHSE <hemedvet@yahoo.com>
2025-08-09 12:27:03 -0300

    definitions

---
+++ FormatAsm/make_ce.bat
@@ -0,0 +1,10 @@
+@echo off
+if exist "FormatAsm.obj" del "FormatAsm.obj"
+if exist "FormatAsm.dll" del "FormatAsm.dll"
+\masm32\bin64\ml64 /c /D@ws=8 /Deditor=1 "FormatAsm.asm"
+\masm32\bin64\Link /SUBSYSTEM:CONSOLE /entry:PluginEntryPoint /DLL /DEF:"FormatAsm_CE.def" "FormatAsm.obj" 
+copy FormatAsm.dll \masm32\plugins64\FormatAsm.dll
+del "FormatAsm.obj"
+del "FormatAsm.exp"
+dir "FormatAsm.*"
+pause
